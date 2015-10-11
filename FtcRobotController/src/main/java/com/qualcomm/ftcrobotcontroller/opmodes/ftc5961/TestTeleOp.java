27df8dca9bf571ca645c72e114d2172cec7c9df7
alden <aldenrogers0@gmail.com>
2015-10-10 17:05:06 -0700

    Add controls for sliding hook

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/TestTeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/TestTeleOp.java
@@ -9,6 +9,7 @@ public class TestTeleOp extends OpMode {
     private DcMotor leftBack;
     private DcMotor rightFront;
     private DcMotor rightBack;
+    private DcMotor slide;
     private TankDrive drive;
     private OrientationTracker ot;
 
@@ -18,6 +19,7 @@ public class TestTeleOp extends OpMode {
         leftBack = hardwareMap.dcMotor.get("M3");
         rightFront = hardwareMap.dcMotor.get("M2");
         rightBack = hardwareMap.dcMotor.get("M4");
+        slide = hardwareMap.dcMotor.get("M5");
         rightFront.setDirection(DcMotor.Direction.REVERSE);
         rightBack.setDirection(DcMotor.Direction.REVERSE);
         drive = new TankDrive();
@@ -34,10 +36,13 @@ public class TestTeleOp extends OpMode {
     public void loop() {
         drive.setPower(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
 
-        // Send orientation data to driver station
-        telemetry.addData("azimuth", ot.orientation[0]);
-        telemetry.addData("pitch", ot.orientation[1]);
-        telemetry.addData("roll", ot.orientation[2]);
+        if (gamepad1.dpad_up) {
+            slide.setPower(0.3);
+        } else if (gamepad1.dpad_down) {
+            slide.setPower(-0.3);
+        } else {
+            slide.setPower(0);
+        }
     }
 
     @Override
