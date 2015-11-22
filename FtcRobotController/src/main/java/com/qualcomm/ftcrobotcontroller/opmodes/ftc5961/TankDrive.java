abf024ee09cd4cd20ba95441ce4538ea62d25dcb
alden <aldenrogers0@gmail.com>
2015-11-21 22:42:12 -0800

    Implement PI controller for heading-based driving
    The current coefficients work rather well, but the robot
    may oscillate out of control if its course is severely
    disrupted.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/TankDrive.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/TankDrive.java
@@ -8,10 +8,21 @@ import java.util.Set;
 public class TankDrive {
     private Set<DcMotor> leftMotors;
     private Set<DcMotor> rightMotors;
+    private int currentId;
+    private double errorAccumulator;
+    private double Kproportional;
+    private double Kintegral;
 
     public TankDrive() {
         leftMotors = new HashSet<DcMotor>();
         rightMotors = new HashSet<DcMotor>();
+        // It's not a problem that the first directed drive maneuver might have
+        // id 0, because 0 is the value to which errorAccumulator is reset upon
+        // a change in id.
+        currentId = 0;
+        errorAccumulator = 0;
+        Kproportional = 0.01;
+        Kintegral = 0.001;
     }
 
     public void addLeftMotor(DcMotor m) {
@@ -28,6 +39,50 @@ public class TankDrive {
         }
         for (DcMotor r : rightMotors) {
             r.setPower(right);
+        }
+    }
+
+    public void setPowerQuiet(double left, double right) {
+        if (left > 1) {
+            left = 1;
+        } else if (left < -1) {
+            left = -1;
+        }
+        if (right > 1) {
+            right = 1;
+        } else if (right < -1) {
+            right = -1;
+        }
+        setPower(left, right);
+    }
+
+    /**
+     * Drive in a particular direction. The ratio between power on the left and
+     * right sides will be such that the robot turns toward the target heading.
+     * This method is expected to be called repeatedly to respond to changes in
+     * heading. In order to use proportional-integral control, so it saves some
+     * data across repeated invocations. Pass a new {@code id} to discard saved
+     * data when targeting a different heading.
+     * @param power the maximum power to assign to one side or the other
+     * @param heading the current heading, in degrees
+     * @param target the desired heading, in degrees
+     * @param id an identifying number unique to the current drive maneuver
+     */
+    public void directed(double power, double heading, double target, int id) {
+        if (id != currentId) {
+            errorAccumulator = 0;
+            currentId = id;
+        }
+        double error = AdafruitIMU.differenceMod(heading, target, 360);
+        if (error > 180) {
+            error -= 360;
+        }
+        errorAccumulator += error;
+        double u = Kproportional * error + Kintegral * errorAccumulator;
+        if (u > 0) {
+            setPowerQuiet((1 - u) * power, power);
+        } else {
+            setPowerQuiet(power, (1 + u) * power);
         }
     }
 
--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/TestingRobot.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/TestingRobot.java
@@ -51,6 +51,17 @@ public class TestingRobot {
         imu = new AdafruitIMU(dim2, 1);
     }
 
+    /**
+     * Convenience method to call
+     * drive.{@link TankDrive#directed(double, double, double, int)}
+     * using imu.{@link AdafruitIMU#relativeHeading()} as the current heading
+     * @param power the maximum drive power
+     * @param targetHeading the desired heading, in degrees
+     */
+    public void driveDirected(double power, double targetHeading, int id) {
+        drive.directed(power, imu.relativeHeading(), targetHeading, id);
+    }
+
     public double getArmPosition() {
         return Math.PI * (960 + (arm.getCurrentPosition() - armOffset)) / 1080;
     }
