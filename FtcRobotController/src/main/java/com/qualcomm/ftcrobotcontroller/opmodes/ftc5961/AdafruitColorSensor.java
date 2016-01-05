1179f40733e473c3510e84fb7f7082de5dd557d1
alden <aldenrogers0@gmail.com>
2016-01-04 17:18:09 -0800

    Correct AdafruitColorSensor register addresses
    Though registers are addressed in a five-bit space, the
    command register to which they are written at the start
    of transactions requires bit 7 to be high. In effect,
    this means that register addresses are 0x80 larger than
    their documented values.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/AdafruitColorSensor.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ftc5961/AdafruitColorSensor.java
@@ -4,17 +4,17 @@ import com.qualcomm.robotcore.hardware.I2cController;
 
 public class AdafruitColorSensor {
     public static final int I2C_ADDRESS = 0x29;
-    public static final int ENABLE = 0x00;
+    public static final int ENABLE = 0x80 | 0x00;
     public static final byte ENABLE_PON = 0b1;
     public static final byte ENABLE_AEN = 0b10;
-    public static final int CDATAL = 0x14;
-    public static final int CDATAH = 0x15;
-    public static final int RDATAL = 0x16;
-    public static final int RDATAH = 0x17;
-    public static final int GDATAL = 0x18;
-    public static final int GDATAH = 0x19;
-    public static final int BDATAL = 0x1A;
-    public static final int BDATAH = 0x1B;
+    public static final int CDATAL = 0x80 | 0x14;
+    public static final int CDATAH = 0x80 | 0x15;
+    public static final int RDATAL = 0x80 | 0x16;
+    public static final int RDATAH = 0x80 | 0x17;
+    public static final int GDATAL = 0x80 | 0x18;
+    public static final int GDATAH = 0x80 | 0x19;
+    public static final int BDATAL = 0x80 | 0x1A;
+    public static final int BDATAH = 0x80 | 0x1B;
 
     private I2cWrapper i2c;
 
