b1470538c421d4d7ee1092953aacf29b3debb717
alden <aldenrogers0@gmail.com>
2016-01-16 23:26:16 -0800

    Last commit
    This was the state of the repository at the
    end of our final competition.
    Changes include:
    - Add autonomous programs
    - Reflect robot hardware changes
    - Update TeleOp accordingly

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -31,6 +31,9 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.ftcrobotcontroller.opmodes.ftc5961.AutonomousBlue;
+import com.qualcomm.ftcrobotcontroller.opmodes.ftc5961.AutonomousRed;
+import com.qualcomm.ftcrobotcontroller.opmodes.ftc5961.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeManager;
 import com.qualcomm.robotcore.eventloop.opmode.OpModeRegister;
 
@@ -46,6 +49,8 @@ public class FtcOpModeRegister implements OpModeRegister {
    * @param manager op mode manager
    */
   public void register(OpModeManager manager) {
-    manager.register("NullOp", NullOp.class);
+    manager.register("Blue Autonomous", AutonomousBlue.class);
+    manager.register("Red Autonomous", AutonomousRed.class);
+    manager.register("TeleOp", TeleOp.class);
   }
 }
